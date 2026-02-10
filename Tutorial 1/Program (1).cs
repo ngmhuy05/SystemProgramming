05483c02d66ae50e560c60de95aa0e8f7e77565a
ngmhuy05 <nguyenhuy142005@gmail.com>
2026-02-11 01:42:51 +0700

    Add files via upload

---
+++ Tutorial 1/Program (1).cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("OS Version: " + Environment.OSVersion);
+        Console.WriteLine("Current Directory: " + Environment.CurrentDirectory);
+        Console.WriteLine("System Time: " + DateTime.Now);
+
+        Console.ReadLine();
+    }
+}
+
